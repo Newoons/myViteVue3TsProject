fe815eac2da3a04a68be99fb25f68f3f1a89790b
Newoons <1518672422@qq.com>
2023-03-17 11:52:10 +0800

    安装vue router 、vuex 、axios、

--- vite-vue3Tsproject/src/main.ts
+++ vite-vue3Tsproject/src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementPlus from "element-plus"
+import "element-plus/dist/index.css"
+import "./assets/css/common.scss"
+import axios from "axios"
 
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
